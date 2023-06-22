f187991848b62bc8c990014caefa598b46b560c6
notdien <thedien7000@gmail.com>
2023-06-21 21:55:52 -0400

    added axios

--- frontend/buff_react/src/pages/create.js
+++ frontend/buff_react/src/pages/create.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios"
-import { Form, FloatingLabel } from 'react-bootstrap'
+import { Form, FloatingLabel, Button } from 'react-bootstrap';
 
 
 export default function Create() {
@@ -16,24 +16,110 @@ export default function Create() {
     const [error, setError] = useState("")
     const [loading, setLoading] = useState("")
 
+    function clear() {
+        document.getElementById("name").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("gender").value = "";
+        document.getElementById("weight").value = "";
+        document.getElementById("height").value = "";
+    }
+
+    let createList = (e) => {
+        e.preventDefault();
+        console.log(Name, Age, Gender, Weight, Height);
+
+        setError(null);
+        setLoading(Name, Age, Gender, Weight, Height);
+
+        axios.post('http://127.0.0.1:5000/create-list', {
+            Name: Name,
+            Age: Age,
+            Gender: Gender,
+            Weight: Weight,
+            Height: Height
+        })
+        .then(function (response) {
+            // successfully created
+            setError(false);
+            setLoading(false);
+            setResponse(response);
+            
+            console.log("The response to the front end is ");
+            console.dir(response);
+            console.log("Response Data: " + response.data)
+            clear();
+        })
+        .catch(function (error) {
+            // for errors
+            setResponse(error);
+            setLoading(false);
+            setError(true);
+            console.log("Error: " + error);
+            clear();
+        })
+        .then(function () {
+            clear();
+        })
+    }
+
     return (
         <React.Fragment>
         <h1>
             Let's create a new list!
         </h1>
 
-        <>
+        <div>
+            <form>
+                
             <FloatingLabel
                 controlId="floatingInput"
                 label="Name"
                 className="mb-3"
+                id="name"
             >
                 <Form.Control type="name" placeholder="John Doe" />
             </FloatingLabel>
-            <FloatingLabel controlId="age" label="Age">
+
+            <FloatingLabel
+                controlId="floatingInput"
+                label="Age"
+                className="mb-3"
+                id="age"
+            >
                 <Form.Control type="age" placeholder="Age" />
             </FloatingLabel>
-        </>
+
+            <FloatingLabel
+                controlId="floatingInput"
+                label="Gender"
+                className="mb-3"
+                id="gender"
+            >
+                <Form.Control type="gender" placeholder="Gender" />
+            </FloatingLabel>
+
+            <FloatingLabel
+                controlId="floatingInput"
+                label="Weight"
+                className="mb-3"
+                id="weight"
+            >
+                <Form.Control type="weight" placeholder="Weight" />
+            </FloatingLabel>
+
+            <FloatingLabel
+                controlId="floatingInput"
+                label="Height"
+                className="mb-3"
+                id="height"
+            >
+                <Form.Control type="height" placeholder="Height" />
+            </FloatingLabel>
+
+            <Button variant="success">Submit</Button>{' '}
+
+            </form>
+        </div>
 
         </React.Fragment>
     );
