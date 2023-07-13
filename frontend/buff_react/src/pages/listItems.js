f22dbecd813c0c3dbff2f7b7ddc2ffac5e8a0496
notdien <thedien7000@gmail.com>
2023-07-13 15:53:10 -0400

    started on view

--- frontend/buff_react/src/pages/listItems.js
+++ frontend/buff_react/src/pages/listItems.js
@@ -1,21 +1,44 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Button } from 'react-bootstrap';
+import axios from "axios";
+import './display.css';
 
 export default function List() {
-    console.log("Listing page has been called!")
+    let [id, setid] = useState('')
+
+
+    let [data, setData] = useState('')
+    let [response, setResponse] = useState('')
+    const [loading, setLoading] = useState('')
+    const [error, setError] = useState('')
+
+    function listItems() {
+        axios.get('http://127.0.0.1:5000/list/' + id)
+        .then(function (response){
+            setError(false);
+            setLoading(false);
+            setData(response.data)
+            console.dir(response.data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        }) 
+    }
 
     return (
         <React.Fragment>
         <div id="container">
-            <form id="default-form">
+            <form onSubmit={listItems} id="default-form">
                 <h1>
                     View your information
                 </h1>
                 <label htmlFor="ID">Enter ID:</label>
-                <input type="text" placeholder="Enter ID"></input>
+                <input required type="text" placeholder="Enter ID"
+                id='id' name='id' value={id}
+                onChange={(e) => setid(e.target.value)}
+                ></input>
 
-                {/* <Button type="submit" variant ="success">{loading ? "submitting" : "Submit"}</Button> */}
-                <Button type="submit">Submit</Button>
+                <Button type="submit">{loading ? "submitting" : "Submit"}</Button>
             </form>
         </div>
         </React.Fragment>
